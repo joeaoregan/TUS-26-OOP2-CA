e612ecd8476ee43b43d218fe95cc21db51457645
joeaoregan <joe.oregan@gmail.com>
2026-04-19 15:12:23 +0100

    Java25: update pom.xml and test java version output from main class

---
+++ guitarrental/src/main/java/com/tus/guitarrental/GuitarRentalApplication.java
@@ -0,0 +1,10 @@
+package com.tus.guitarrental;
+
+public class GuitarRentalApplication {
+	public static void main(String[] args) {
+		System.out.println("=======================================");
+		System.out.println("   The Six-String Hub: Guitar Rental   ");
+		System.out.println("=======================================");
+		System.out.println("Java Version: " + System.getProperty("java.version"));
+	}
+}
--- guitarrental/src/main/java/com/tus/guitarrental/app/GuitarRentalApplication.java
+++
@@ -1,9 +0,0 @@
-package com.tus.guitarrental.app;
-
-public class GuitarRentalApplication {
-	public static void main(String[] args) {
-		System.out.println("Welcome to the Guitar Rental Application!");
-		// You can add code here to initialize your application, such as creating instances of guitars, handling user input, etc.
-	}
-
-}
